375d1c6c39061929550f3c4c4e62d69385c33dcb
Tibor Kranjčec <tibor.kranjcec@verybigthings.com>
2021-08-12 12:31:58 +0200

    Update deps, remove unnecessary ones, remove not used fns from app

--- src/app/app.cljs
+++ src/app/app.cljs
@@ -3,15 +3,6 @@
             [keechma.next.controllers.dataloader]
             [keechma.next.controllers.subscription]
             ["react-dom" :as rdom]))
-
-(defn page-eq? [page] (fn [{:keys [router]}] (= page (:page router))))
-
-(defn role-eq? [role] (fn [deps] (= role (:role deps))))
-
-(def homepage? (page-eq? "home"))
-
-(defn slug [{:keys [router]}] (:slug router))
-
 (def app
   {:keechma.subscriptions/batcher rdom/unstable_batchedUpdates,
    :keechma/controllers
